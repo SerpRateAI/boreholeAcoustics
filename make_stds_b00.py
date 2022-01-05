06f6c4f717bd0a4d56f8b9970dcf767c62e9887d
john <johnm.aiken@gmail.com>
2022-01-05 22:33:00 +0100

    created new python file for creating all the stds data

---
+++ make_stds_b00.py
@@ -0,0 +1,75 @@
+"""
+This file calculates the standard deviations of the noise levels for data from hole b1
+"""
+import pandas as pd
+import numpy as np
+import obspy
+import glob
+from datetime import datetime
+
+def get_stream(paths):
+    """
+    opens the data as an obspy stream from the given list of paths
+    """
+    stream = obspy.read(paths[0])
+    for p in paths[1:]:
+        stream = stream + obspy.read(p)       
+    stream[3].data = -1 * stream[3].data
+    return stream
+
+def get_std(stream, length_step):
+    """
+    creates a pandas dataframe with the standard deviations calculated for each hydrophone
+    for 60s non-overlapping windows.
+    """
+    stream_copy = stream.copy()
+    dfstd = pd.DataFrame()
+    for window in stream_copy.slide(window_length=length_step, step=length_step):
+        dfwindow = pd.DataFrame(window.std()).transpose()
+        dfwindow.index = (window[0].stats['starttime'],)
+        dfstd = pd.concat([dfstd, dfwindow])
+    dfstd.columns = ['h0', 'h1', 'h2', 'h3', 'h4', 'h5']
+    return dfstd
+
+def get_std_for_one_day(dataloc):
+    """
+    wrapper function for get_stream and get_std that writes to file the standard deviation
+    calculated curves for a single day.
+    """
+    day = dataloc.split('.')[-1]
+    year = dataloc.split('.')[-2]
+    stream = get_stream(paths=dataloc)
+    stream.filter(freqmin=6.0, freqmax=8.0, type='bandpass')
+    std = get_std(s, length_step=60.0)
+    std['day'] = day
+    std['year'] = year
+    std.to_csv('/media/sda/data/robdata/stds/std_{year}_{day}.csv', index=True)
+    del day, year, stream, std
+
+def create_datafiles():
+    """
+    creates a list of lists of datafiles, there are 6 files per day for each of 6 hydrophones
+    """
+    datafiles = []
+    years_days = [d.split('.')[-2:] for d in glob.glob('/media/sda/data/robdata/Hydrophones/DAYS/B00/*')]
+    years_days = np.array([list(x) for x in set(tuple(x) for x in years_days)])
+    
+    for y, d in years_days:
+        day_dirs = []
+        for s in [1,2,3,4,5,6]:
+            dir = '/media/sda/data/robdata/Hydrophones/DAYS/B00/B00.7F.0{s}.GDH.{y}.{d}'.format(s=s, y=y, d=d)
+            day_dirs.append(dir)
+        datafiles.append(day_dirs)  
+    return datafiles
+
+if __name__=='__main__':
+    start = datetime.now()
+    print('process started at', str(start))
+    
+    datafiles = create_datafiles()
+    # create a processor pool to ingest the data, by default uses all processors, we use 10 here
+    pool = Pool(10)
+    pool.map(get_std_for_one_day, datafiles)
+    pool.close()
+    print('process finished at', datetime.now()-start)
+    print('detection tables and similiarity scores can be found at',  '/media/sda/data/borehole/detections/')
